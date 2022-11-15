2147ec6b3c77404c7183cb2075b8f8e582d4a9f6
Marek Polek <marek.pawel.polek@gmail.com>
2022-11-15 09:31:08 +0100

    add simple - service and controller for publisher

---
+++ src/main/java/pl/boardgame/app/service/PublisherService.java
@@ -0,0 +1,4 @@
+package pl.boardgame.app.service;
+
+public interface PublisherService {
+}
---
+++ src/main/java/pl/boardgame/app/service/imp/PublisherServiceImp.java
@@ -0,0 +1,7 @@
+package pl.boardgame.app.service.imp;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PublisherServiceImp {
+}
